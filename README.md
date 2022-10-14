06128d6479dd80b8638ee7ccde6ef88d3f880c86
zhitm <zhitnuhina.maria@gmail.com>
2022-10-14 22:16:07 +0300

    added task2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # C# SPBU homeworks
+
 ## Житнухина Мария
---
+++ Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Immutable;
+using System.Text;
+
+namespace Task2;
+
+public class Program
+{
+    public static (string, int) Bwt(string str)
+    {
+        var result = "";
+        var suffixArray = GetSuffixIndices(str + str);
+        suffixArray = suffixArray.Where(x => x < str.Length).ToArray();
+        foreach (var index in suffixArray)
+        {
+            if (index == 0)
+            {
+                result += str.Last();
+                continue;
+            }
+
+            result += str[index - 1];
+        }
+
+        return (result, Array.IndexOf(suffixArray, 0));
+    }
+
+    public static string ReverseBwt(string str, int initialPosition)
+    {
+        var result = "";
+        var alphabet = new List<char>();
+        foreach (var el in str)
+        {
+            if (!alphabet.Contains(el))
+            {
+                alphabet.Add(el);
+            }
+        }
+
+        alphabet.Sort();
+
+        var frequency = new int[alphabet.Count];
+        foreach (var el in str)
+        {
+            frequency[alphabet.IndexOf(el)] += 1;
+        }
+
+        var charPositionsInSortArray = new int[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            charPositionsInSortArray[i] = frequency.Take(alphabet.IndexOf(str[i])).Sum() +
+                                          str.Take(i).Count(x => x == str[i]);
+        }
+
+        var currentPosition = initialPosition;
+        while (result.Length != str.Length)
+        {
+            result = str[currentPosition] + result;
+            currentPosition = charPositionsInSortArray[currentPosition];
+        }
+
+        return result;
+    }
+
+    private static int[] GetSuffixIndices(string str)
+    {
+        var array = Enumerable.Range(0, str.Length).ToArray();
+        Array.Sort(array, (i, j) => string.Compare(str[i..], str[j..], StringComparison.Ordinal));
+        return array;
+    }
+
+    public static void Main()
+    {
+    }
+}
