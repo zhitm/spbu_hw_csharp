d0de1ba79485a6161677137790ad853e71a30423
zhitm <zhitnuhina.maria@gmail.com>
2022-11-12 15:22:10 +0300

    queue implemented

---
+++ Test/MySafePriorityQueue.cs
@@ -0,0 +1,36 @@
+﻿namespace Test;
+
+public class MySafePriorityQueue<T, K> where K : IComparable<K>
+{
+    private SortedList<K, T> _list = new SortedList<K, T>();
+    private int _size;
+    private readonly object _locker = new();
+
+    public void Enqueue(T value, K priority)
+    {
+        lock (_locker)
+        {
+            _list.Add(priority, value);
+            _size++;
+            Monitor.Pulse(_locker);
+        }
+    }
+
+    public T Dequeue()
+    {
+        lock (_locker)
+        {
+            if (_list.Count == 0)
+            {
+                Monitor.Wait(_locker);
+            }
+
+            var el = _list.Values.Last();
+            _list.RemoveAt(_list.Count - 1);
+            _size--;
+            return el;
+        }
+    }
+
+    public int Size => _size;
+}
---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Test;
+
+class Program
+{
+    public static void Main()
+    {
+        var queue = new MySafePriorityQueue<string, int>();
+        var t = Task.Run(() => queue.Dequeue());
+        Task.Run((() => queue.Enqueue("11", 10)));
+        Console.WriteLine(t.Result);
+        // queue.Enqueue("11", 10);
+        // queue.Enqueue("1111", 0);
+        // Console.WriteLine(queue.Dequeue());
+    }
+}
